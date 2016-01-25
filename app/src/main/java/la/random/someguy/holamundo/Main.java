ea185b1083c90c138993c92d75a84ccc3ff07915
Some Guy <someguy@random.la>
2016-01-25 14:47:09 -0800

    Removed the main.xml and created a runtime activity (ListActivity) - edits to Android Manifest to correspond

--- app/src/main/java/la/random/someguy/holamundo/Main.java
+++ app/src/main/java/la/random/someguy/holamundo/Main.java
@@ -2,12 +2,20 @@ package la.random.someguy.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.activity_main);
+        Context context = Main.this;
+        Class target = la.random.someguy.holamundo.activities.MenuList.class;
+        Intent intent = new Intent(context, target);
+        startActivity(intent);
+        finish();
     }
 }
